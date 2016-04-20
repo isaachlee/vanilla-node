b867b9554a8c1ecc3a3b7fa127d57e1a3a99aa2e
Isaac Lee <kioshuu3371@gmail.com>
2016-04-20 11:17:29 -0700

    set up server as a module and called it using index.js

--- server.js
+++ server.js
@@ -1,7 +1,19 @@
+// use http module included in nodejs
 var http = require("http");
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+// create function to create server so it can be used for asynchronus callbacks.
+function start(port) {
+  var serverport = port
+  function onRequest(request, response) {
+    console.log("Request Recieved")
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(serverport);
+
+  console.log("Server has started. Listening on 127.0.0.1:" + serverport)
+}
+
+exports.start = start;
